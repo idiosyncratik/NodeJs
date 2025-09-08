2c032ba7859cbb7f1b2c6d4f6c1c297c1ace0fee
Yas <111201770+idiosyncratik@users.noreply.github.com>
2025-09-08 15:21:41 -0300

    Delete mod1 directory
    Me enganei com uma coisa.. Vou reenviar.

--- mod1/atv1aula5.js
+++
@@ -1,6 +0,0 @@
-const entrada = require('prompt-sync')({sigint: false});
-const n = Number(console.log("Digite um número: "));
-
-for (let i = 1; i <= n; i++) {
-  console.log(i)
-}
--- mod1/atv2aula3.js
+++
@@ -1,7 +0,0 @@
-const entrada = require('prompt-sync')({ sigint: false });
-
-let dias = entrada("Digite quantos dias de atraso: ");
-dias = Number(dias);
-multa = (dias*1.50);
-console.log("Sua multa está no valor de: " + (multa) + ",00 R$.");
-
